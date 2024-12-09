cc077236315b433b95634e472d38ca9b52f178a5
Gaurav (wsl) <phuyalgaurav90@gmail.com>
2024-12-09 16:10:16 +0000

    add static files including SVG icons, JavaScript initialization, and font README documentation

--- project4/wsgi.py
+++ project4/wsgi.py
@@ -1,16 +1,8 @@
-"""
-WSGI config for project4 project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
-"""
-
+# filepath: /home/g/dev/crasber/project4/wsgi.py
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project4.settings')
 
 application = get_wsgi_application()
+app = application
